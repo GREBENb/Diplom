00dcf71971ea97c6b8c4430791cbca29f65ed90b
GREBENb <grebenb67@rambler.ru>
2014-03-07 11:56:11 +0300

    Revert "111"
    This reverts commit ca77b0dbc2ac8ad9d3785b8a4b7da8f5c459be2d.

--- visual studio/MvcApp/MvcApp/App_Start/BundleConfig.cs
+++ visual studio/MvcApp/MvcApp/App_Start/BundleConfig.cs
@@ -8,41 +8,6 @@ namespace MvcApp
         // Дополнительные сведения о Bundling см. по адресу http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-            //            "~/Scripts/jquery-{version}.js"));
-
-            //bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-            //            "~/Scripts/jquery-ui-{version}.js"));
-
-            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-            //            "~/Scripts/jquery.unobtrusive*",
-            //            "~/Scripts/jquery.validate*"));
-
-
-            //// Используйте версию Modernizr для разработчиков, чтобы учиться работать. Когда вы будете готовы перейти к работе,
-            //// используйте средство построения на сайте http://modernizr.com, чтобы выбрать только нужные тесты.
-            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-            //            "~/Scripts/modernizr-*"));
-
-            //bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
-
-            //bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-            //            "~/Content/themes/base/jquery.ui.core.css",
-            //            "~/Content/themes/base/jquery.ui.resizable.css",
-            //            "~/Content/themes/base/jquery.ui.selectable.css",
-            //            "~/Content/themes/base/jquery.ui.accordion.css",
-            //            "~/Content/themes/base/jquery.ui.autocomplete.css",
-            //            "~/Content/themes/base/jquery.ui.button.css",
-            //            "~/Content/themes/base/jquery.ui.dialog.css",
-            //            "~/Content/themes/base/jquery.ui.slider.css",
-            //            "~/Content/themes/base/jquery.ui.tabs.css",
-            //            "~/Content/themes/base/jquery.ui.datepicker.css",
-            //            "~/Content/themes/base/jquery.ui.progressbar.css",
-            //            "~/Content/themes/base/jquery.ui.theme.css"));
-
-            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Content/bootstrap/js/bootstrap.js"));
-            //bundles.Add(new StyleBundle("~/Content/bootstrap/css").Include("~/Content/bootstrap/css/bootstrap.css"));
-
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
@@ -54,48 +19,30 @@ namespace MvcApp
                         "~/Scripts/jquery.validate*"));
 
 
-            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-            //            "~/Scripts/modernizr-*"));
+            // Используйте версию Modernizr для разработчиков, чтобы учиться работать. Когда вы будете готовы перейти к работе,
+            // используйте средство построения на сайте http://modernizr.com, чтобы выбрать только нужные тесты.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/metro").Include(
-                                                                     "~/Scripts/metro-dialog.js",
-                                                                     "~/Scripts/metro-dropdown.js",
-                                                                     "~/Scripts/metro-accordion.js",
-                                                                     "~/Scripts/metro-buttonset.js",
-                                                                     "~/Scripts/metro-carousel.js",
-                                                                     "~/Scripts/metro-input-control.js",
-                                                                     "~/Scripts/metro-pagecontrol.js",
-                                                                     "~/Scripts/metro-pagelist.js",
-                                                                     "~/Scripts/metro-rating.js",
-                                                                     "~/Scripts/metro-slider.js",
-                                                                     "~/Scripts/metro-tile-slider.js",
-                                                                     "~/Scripts/metro-tile-drag.js",
-                                                                     "~/Scripts/metro-calendar.js"));
-
-
-
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                                                                    "~/Content/metro-bootstrap.css",
-                                                                    "~/Content/metro-bootstrap-responsive.css",
-                                                                    "~/Content/Site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                       "~/Content/themes/base/jquery.ui.core.css",
-                       "~/Content/themes/base/jquery.ui.resizable.css",
-                       "~/Content/themes/base/jquery.ui.selectable.css",
-                       "~/Content/themes/base/jquery.ui.accordion.css",
-                       "~/Content/themes/base/jquery.ui.autocomplete.css",
-                       "~/Content/themes/base/jquery.ui.button.css",
-                       "~/Content/themes/base/jquery.ui.dialog.css",
-                       "~/Content/themes/base/jquery.ui.slider.css",
-                       "~/Content/themes/base/jquery.ui.tabs.css",
-                       "~/Content/themes/base/jquery.ui.datepicker.css",
-                       "~/Content/themes/base/jquery.ui.progressbar.css",
-                       "~/Content/themes/base/jquery.ui.theme.css"));
+                        "~/Content/themes/base/jquery.ui.core.css",
+                        "~/Content/themes/base/jquery.ui.resizable.css",
+                        "~/Content/themes/base/jquery.ui.selectable.css",
+                        "~/Content/themes/base/jquery.ui.accordion.css",
+                        "~/Content/themes/base/jquery.ui.autocomplete.css",
+                        "~/Content/themes/base/jquery.ui.button.css",
+                        "~/Content/themes/base/jquery.ui.dialog.css",
+                        "~/Content/themes/base/jquery.ui.slider.css",
+                        "~/Content/themes/base/jquery.ui.tabs.css",
+                        "~/Content/themes/base/jquery.ui.datepicker.css",
+                        "~/Content/themes/base/jquery.ui.progressbar.css",
+                        "~/Content/themes/base/jquery.ui.theme.css"));
 
-
-            BundleTable.EnableOptimizations = true;
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Content/bootstrap/js/bootstrap.js"));
+            //bundles.Add(new StyleBundle("~/Content/bootstrap/css").Include("~/Content/bootstrap/css/bootstrap.css"));
+           
         }
     }
 }
